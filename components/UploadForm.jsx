6d13608099e7a56b6e233e265070174eb1d08031
Joseph Elias <jrelias@outlook.com>
2023-01-13 15:43:44 -0500

    form updates

--- components/UploadForm.jsx
+++ components/UploadForm.jsx
@@ -19,75 +19,82 @@ registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 const UploadForm = () => {
   const [files, setFiles] = useState([]);
+  const [sell, setSell] = useState(false);
+  const [waterType, setWaterType] = useState('null');
+
+  const handleSale = (e) => {
+    setSell(e.target.value);
+  };
+
   return (
     <form action='/' method='post' enctype='text/plain'>
       <FilePond
         files={files}
         onupdatefiles={setFiles}
-        allowMultiple={true}
-        maxFiles={3}
+        allowMultiple={false}
+        maxFiles={1}
         server='/api'
         name='files' /* sets the file input name, it's filepond by default */
-        labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+        labelIdle='Drag & Drop your picture or <span class="filepond--label-action">Browse</span>'
       />
-      <div className='grid grid-cols-2'>
-        <div>
-          <input type='radio' />
-          <label> Freshwater</label>
-        </div>
-        {/* <input type='radio'>Saltwater</input> */}
-        <div>
-          <input type='radio'></input>
-          <label> Saltwater</label>
-        </div>
-      </div>
       <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
         <div className='flex flex-col'>
-          <label className='uppercase text-sm py-2'>Name</label>
+          <label className='uppercase text-sm py-2'>Scientific Name</label>
+          <input
+            className='border-2 rounded-lg p-3 flex border-gray-300'
+            type='text'
+            name='species'
+            placeholder='e.g: Neocaridina'
+          />
+        </div>
+        <div className='flex flex-col'>
+          <label className='uppercase text-sm py-2'>Common Name</label>
           <input
             className='border-2 rounded-lg p-3 flex border-gray-300'
             type='text'
             name='name'
-          />
-        </div>
-        <div className='flex flex-col'>
-          <label className='uppercase text-sm py-2'>Phone Number</label>
-          <input
-            className='border-2 rounded-lg p-3 flex border-gray-300'
-            type='text'
-            name='phone'
+            placeholder='e.g: Cherry Shrimp'
           />
         </div>
       </div>
       <div className='flex flex-col py-2'>
-        <label className='uppercase text-sm py-2'>Email</label>
-        <input
-          className='border-2 rounded-lg p-3 flex border-gray-300'
-          type='email'
-          name='email'
-        />
+        <label className='uppercase text-sm py-2'>Water Type?</label>
+        <select className='border-2 rounded-lg p-3 border-gray-300'>
+          <option value='Freshwater'>Freshwater</option>
+          <option value='Saltwater'>Saltwater</option>
+        </select>
       </div>
       <div className='flex flex-col py-2'>
-        <label className='uppercase text-sm py-2'>Subject</label>
-        <input
-          className='border-2 rounded-lg p-3 flex border-gray-300'
-          type='text'
-          // name='subject'
-          onChange={(e) => {
-            updateSubject(
-              `mailto:jrelias@outlook.com?subject=` + e.target.value
-            );
-          }}
-        />
-      </div>
-      <div className='flex flex-col py-2'>
-        <label className='uppercase text-sm py-2'>Message</label>
+        <label className='uppercase text-sm py-2'>Notes</label>
         <textarea
           className='border-2 rounded-lg p-3 border-gray-300'
-          rows='10'
-          name='message'
+          rows='5'
+          name='notes'
+          placeholder='Add any interesting notes that you want to about this species here!'
         ></textarea>
       </div>
+      <div className='flex flex-col py-2'>
+        <label className='uppercase text-sm py-2'>For Sale?</label>
+        <select
+          value={sell}
+          onChange={handleSale}
+          className='border-2 rounded-lg p-3 border-gray-300'
+        >
+          <option value={false}>No</option>
+          <option value={true}>Yes</option>
+        </select>
+      </div>
+      {sell === true && (
+        <div className='flex flex-col py-2'>
+          <label className='uppercase text-sm py-2'>Sale Info</label>
+          <textarea
+            className='border-2 rounded-lg p-3 border-gray-300'
+            rows='5'
+            name='notes'
+            placeholder='Add your sale inquery contact information'
+          ></textarea>
+        </div>
+      )}
       <button className='w-full p-4 mt-4 text-gray-100'>Send Message</button>
     </form>
   );
